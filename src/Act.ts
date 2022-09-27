c104e49300d985a6749771b4577bfb574430d1d1
Joshua Horowitz <joshuah@alum.mit.edu>
2022-09-26 19:00:46 -0700

    avoid crash from jaggy-filtering trivial paths

--- src/Act.ts
+++ src/Act.ts
@@ -29,16 +29,18 @@ export class Act {
     if (polyline) {
       let latLngs = mbPolyline.decode(polyline) as [number, number][];  // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/48644
 
-      // filter jaggies
-      const dist1 = _.range(latLngs.length - 1).map(i => Math.hypot(latLngs[i][0] - latLngs[i+1][0], latLngs[i][1] - latLngs[i+1][1]));
-      const dist2 = _.range(latLngs.length - 2).map(i => Math.hypot(latLngs[i][0] - latLngs[i+2][0], latLngs[i][1] - latLngs[i+2][1]));
-      for (let i = latLngs.length - 2; i >= 1; i--) {
-        const AB = dist1[i - 1];
-        const BC = dist1[i];
-        const AC = dist2[i - 1];
-        const excursion = AB + BC / 2;
-        if (AC < 0.3 * excursion) {
-          latLngs.splice(i, 1);
+      if (latLngs.length > 2) {
+        // filter jaggies
+        const dist1 = _.range(latLngs.length - 1).map(i => Math.hypot(latLngs[i][0] - latLngs[i+1][0], latLngs[i][1] - latLngs[i+1][1]));
+        const dist2 = _.range(latLngs.length - 2).map(i => Math.hypot(latLngs[i][0] - latLngs[i+2][0], latLngs[i][1] - latLngs[i+2][1]));
+        for (let i = latLngs.length - 2; i >= 1; i--) {
+          const AB = dist1[i - 1];
+          const BC = dist1[i];
+          const AC = dist2[i - 1];
+          const excursion = AB + BC / 2;
+          if (AC < 0.3 * excursion) {
+            latLngs.splice(i, 1);
+          }
         }
       }
 
